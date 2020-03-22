df6437a117e90e35741b646aa0b5a851471a61bd
Jason Haymond <54720927+jasonhaymond@users.noreply.github.com>
2020-03-21 21:30:30 -0700

    Updated url

--- Raspberry-Pi/Pihole-Deploy/start.sh
+++ Raspberry-Pi/Pihole-Deploy/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-sudo chmod +x ./install.sh
+echo "Installing SVN..."
+sudo apt-get install subversion -y
+
+echo "Downloading installation files..."
+if [ -d "./Pihole-Deploy" ]
+then
+    echo "Folder already exists.  Overwriting..."
+    sudo rm -rf ./Pihole-Deploy
+fi
+svn export https://github.com/jasonhaymond/Linux/trunk/Raspberry-Pi/Pihole-Deploy/
+find ./Pihole-Deploy/ -type f -iname "*.sh" -exec sudo chmod +x {} \;
+cd ./Pihole-Deploy
 sudo ./install.sh | sudo tee ./piholeinstall.log
 finish
 
